f6bc6ffa35fa007561c16d2ed5b34d558d18879e
Amos Chen <amoschen614@gmail.com>
2017-04-27 17:36:32 -0700

    add basic tests for table model

---
+++ client/js/test/table-model.test.js
@@ -0,0 +1,11 @@
+const TableModel = require('../table-model');
+
+describe('table-model', () => {
+	it ('can set then get a value', () => {
+		const model = new TableModel();
+		const location = { row: 3, col: 5 };
+		expect(model.getValue(location)).toBeUndefined();
+		model.setValue(location, 'blah');
+		expect(model.getValue(location)).toBe('blah');
+	});
+});
