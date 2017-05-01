0a65a3821ebb160e14eece64af10ce444c0a1b90
Amos Chen <amoschen614@gmail.com>
2017-05-01 01:59:06 -0700

    add start script to package.json and modified index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
-const express = require('express');
-const app = express();
+var http = require('http');
+var express = require('express');
+var app = express();
 
 app.use(express.static('public'));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
-const server = http.createServer(app);
+var server = http.createServer(app);
 server.listen(app.get('port'), () => {
 	console.log(`Server listening on port ${app.get('port')}...`);
 });
